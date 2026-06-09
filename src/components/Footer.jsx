79aa997762d48789638460bbc26eea3f7ca44896
UddheshyaGroup <uddheshyastudio@gmail.com>
2026-06-09 13:55:15 +0545

    Refactor: removed the twitter button from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -332,11 +332,6 @@ const Footer = () => {
                   url: 'https://instagram.com/'
                 },
                 {
-                  icon: <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" /></svg>,
-                  label: 'Twitter',
-                  url: 'https://twitter.com/'
-                },
-                {
                   icon: <MessageSquare size={18} />,
                   label: 'WhatsApp',
                   url: 'https://wa.me/9779867667837'
